39f710867c70cceb54e0b01b897f679c2c30a80a
Taylor j. Eke <tayloreke@gmail.com>
2025-01-24 12:34:22 -0800

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Taylor on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
